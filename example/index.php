c05f3561001f3b247940172b124d3100425b5777
Dorian <srv023@leafbit.com>
2016-12-03 14:03:17 +0800

    Post, get and query methods updated to support textareas.

--- example/index.php
+++ example/index.php
@@ -1,61 +1,152 @@
-<html><body>
-<h1>
-The Green People Book Club
-</h1>
+<html>
+<head>
+  <title>PHP CLI Broser Scirpt Example</title>
+  <style>
+  .message {
+    border: 1px solid green;
+    background: lightgreen;
+  }
+  </style>
+</head>
+<body>
+<h1>PHP Script Example</h1>
 
 <?php
-if ($_POST) {
-  print_r($_POST);
+function show_message($text, $vals) {
+  if (!$vals) return;
+  print '<div class="message">' . $text . '<br />';
+  print_r($vals);
+  print '</div>';
 }
+// If anything send by $_POST or $_GET - show:
+show_message('A post event received. It contains:', $_POST);
+show_message('A get event received. It contains:', $_GET);
+echo '<p>A random number: ' . rand(0, 999999) . '</p>';
 ?>
 
 <p>
-Welcome to The Green People Book Club. Please register to obtain a membership with us.
+<a href="index.php?argument=daa&andarg=noo">Test Link</a> - click on this link to test get method.
 </p>
-    <form action="index.php" data-onsubmit="phpBrowser.submit(this);return false;">
-    <table>
-    <tbody><tr>
-        <td>
-        First name:
-        </td>
-        <td>
-            <input type="text" name="firstname" id="firstname">
-        </td>
-    </tr>
-    <tr>
-        <td>
-        Last name:
-        </td>
-        <td>
-            <input type="text" name="lastname" id="lastname">
-        </td>
-    </tr>
-    <tr>
-        <td>
-        Gender:
-        </td>
-        <td>
-        <input type="radio" name="gender" id="genderMale" value="Male"> Male
-        <input type="radio" name="gender" id="genderFemale" value="Female"> Female
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2">
-        <input type="checkbox" name="updates" id="updates" value="receive">
-        Check here if you would like to receive regular updates from us:
-        </td>
-    </tr>
-    </tbody></table>
-    <input type="submit" value="Submit">
-    </form>
 
-    <p>
-    <a href="another.php?argument=daa&andarg=noo">Test Link</a>
-    </p>
-    <script>
-    function IWantToDo(){
-      formExtractor.goto('This is where I want to go');
-    }
-    </script>
-    <input type="button" onclick="IWantToDo()" value="Shmksss" />
-</body></html>
+<p>
+  An example of form to test GET method:
+</p>
+    <form action="index.php">
+    <table>
+    <tbody>
+      <tr>
+        <td>First name:</td>
+        <td><input type="text" name="person[firstname]" value="Ali"></td>
+      </tr>
+      <tr>
+        <td>Last name:</td>
+        <td><input type="text" name="person[lastname]" value="Bl'ahblah&"></td>
+      </tr>
+      <tr>
+        <td>Gender:</td>
+        <td>
+          <input type="radio" name="gender" value="Male" checked="checked"> Male
+          <input type="radio" name="gender" value="Female"> Female
+          </td>
+      </tr>
+      <tr>
+        <td>Source:</td>
+        <td><select name="source">
+          <option>Sand</option>
+          <option>Water</option>
+          <option>Fire"</option>
+          <option>Earth</option>
+          <option>Wood</option>
+          <option>Milk</option>
+          </select>
+          </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <input type="checkbox" name="checkboxtest" value="receive">
+          Checkbox test
+          </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <textarea name="multiline" cols=60 rows=6>
+This is a long long story. Years and years ago
+before computers and other electronic hardware
+existed one script written on stone plates.
+
+It was well formatted, well compiled and well done.
+So what?
+          </textarea>
+          </td>
+      </tr>
+      <tr>
+        <td>Upload Something?</td>
+        <td><input type="file" name="afile" /></td>
+      </tr>
+    </tbody>
+    </table>
+    <input type="submit" value="Submit this now!">
+    </form>
+<p>
+  An example of form to test POST method:
+</p>
+    <form action="index.php" method="POST" enctype="multipart/form-data">
+     <table>
+    <tbody>
+      <tr>
+        <td>First name:</td>
+        <td><input type="text" name="firstname" value="Aliya"></td>
+      </tr>
+      <tr>
+        <td>Last name:</td>
+        <td><input type="text" name="lastname" value="Blahblaha"></td>
+      </tr>
+      <tr>
+        <td>Gender:</td>
+        <td>
+          <input type="radio" name="gender" value="Male"> Male
+          <input type="radio" name="gender" value="Female" checked="checked"> Female
+          </td>
+      </tr>
+      <tr>
+        <td>Source:</td>
+        <td><select name="source">
+          <option>Sand</option>
+          <option>Water</option>
+          <option selected=selected>Fire</option>
+          <option>Earth</option>
+          <option>Wood</option>
+          <option>Milk</option>
+           </select>
+            </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <input type="checkbox" name="checkboxtest" value="receive">
+          Checkbox test
+          </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          <textarea name="multiline" cols=60 rows=6>
+This is a long long story. Years and years ago
+before computers and other electronic hardware
+existed two script written on golden plates.
+
+It was well formatted, well compiled and well done.
+So... one day it got run.
+          </textarea>
+          </td>
+      </tr>
+      <tr>
+        <td>Upload this too?</td>
+        <td><input type="file" name="afile2" />
+No no no - it will result in C:\fakepath\urfilename
+</td>
+      </tr>
+    </tbody>
+    </table>
+    <input type="submit" value="Submit me! Test Me!">
+   </form>
+</body>
+</html>
--- globals.php
+++ globals.php
@@ -3,8 +3,9 @@ global $_SESSION, $_POST;
 $_POST = array();
 
 $session_file = dirname(__FILE__) . '/default.session';
-$post_file = dirname(__FILE__) . '/post.php';
-$get_file = dirname(__FILE__) . '/get.php';
+$post_file = dirname(__FILE__) . '/post';
+$get_file = dirname(__FILE__) . '/get';
+$query_file = dirname(__FILE__) . '/query';
 
 if (file_exists($session_file)) {
   $_SESSION = unserialize(file_get_contents($session_file));
@@ -12,13 +13,69 @@ if (file_exists($session_file)) {
   $_SESSION = array();
 }
 
-if (file_exists($get_file)) {
-  include($get_file);
-  unlink($get_file);
+/**
+ * Parses the raw field key into an array field key code.
+ */
+function __globals__fieldK($rawk) {
+    $k = preg_replace(array("/\[/", "/\]/", "/^\w+/"),
+      array("['", "']", "['\$0']"), $rawk);
+    return $k;
+}
+/**
+ * Loads field values from a file recorded by phpCliBroser program.
+ */
+function getFieldsFromFile($fname){
+  $flds = array();
+  if (!file_exists($fname)) return array();
+  $lines = file($fname);
+  $isTextArea = FALSE;
+  foreach($lines as $ll) {
+    $l = trim($ll);
+    
+    if (preg_match("/^<\!CDATA\+\+\+ textarea start\:(.*?)$/", $l, $mt)){
+      $isTextArea = TRUE;
+      $k = __globals__fieldK($mt[1]);
+      eval("\$flds$k='';");
+      eval("\$textAreaVal = &\$flds$k;");
+      continue;
+    }
+
+    if ($l == '>>textarea end]]+++>'){
+      $isTextArea = FALSE;
+      eval("\$flds$k = trim(\$flds$k);");
+      continue;
+    }
+
+    if ($isTextArea) {
+      //$textAreaVal += $ll;
+      $code = "\$flds$k .= \$ll;";
+      eval($code);
+      continue;
+    }
+
+    if (preg_match("/^FLD\:(.*?)\=(.*?)$/", $l, $mt)) {
+      $k = __globals__fieldK($mt[1]);
+      if (!$k) continue;
+      $code = "\$flds$k=\$mt[2];";
+      eval($code);
+    }
+
+  }
+  unlink($fname);
+  return $flds;
 }
 
-if (file_exists($post_file)) {
-  include($post_file);
-  unlink($post_file);
+$_GET = array();
+if (file_exists($query_file)) {
+  $query = trim(file_get_contents($query_file));
+  foreach (explode('&', $query) as $chunk) {
+    $param = explode("=", $chunk);
+    $k = __globals__fieldK($param[0]);
+    if (!$k) continue;
+    eval("\$_GET$k = \$param[1];");
+  }
+  unlink($query_file);
 }
+$_GET += getFieldsFromFile($get_file);
+$_POST = getFieldsFromFile($post_file);
 ?>
